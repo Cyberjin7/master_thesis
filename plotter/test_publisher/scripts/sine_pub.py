9d58a417256eacef2a325257b6e5c8f37905a84f
Jin-Ho Lee <jinho.lee@tum.de>
2023-01-24 19:17:59 +0100

    Plotting time versetzt in plotjuggler

---
+++ plotter/test_publisher/scripts/sine_pub.py
@@ -0,0 +1,24 @@
+#! /usr/bin/ev python
+
+import rospy
+from std_msgs.msg import Float64
+from custom_ros_msgs.msg import CustomData
+import numpy as np
+
+
+
+if __name__ == '__main__':
+    pub = rospy.Publisher('sine', CustomData, queue_size=100)
+    rospy.init_node('sine_publisher', anonymous=True)
+    rate = rospy.Rate(20)
+
+    sine = CustomData()
+
+    start_time = rospy.get_rostime()
+
+    while not rospy.is_shutdown():
+        ros_time = rospy.get_rostime() - start_time
+        sine.header.stamp = ros_time
+        sine.value.data = np.sin(ros_time.to_sec())
+        pub.publish(sine)
+        rate.sleep()
---
+++ plotter/test_publisher/scripts/sine_pub2.py
@@ -0,0 +1,28 @@
+#! /usr/bin/ev python
+
+import rospy
+from std_msgs.msg import Float64
+from custom_ros_msgs.msg import CustomData
+import numpy as np
+
+
+
+if __name__ == '__main__':
+    pub = rospy.Publisher('sine2', CustomData, queue_size=100)
+    rospy.init_node('sine_publisher2', anonymous=True)
+    rate = rospy.Rate(20)
+
+    sine = CustomData()
+    start_time = rospy.get_rostime()
+
+    delay = rospy.get_param("~delay")
+    rospy.loginfo("Delay is %s seconds.", delay)
+
+    while not rospy.is_shutdown():
+
+        ros_time = rospy.get_rostime() - start_time
+        ros_time.secs += delay
+        sine.value.data = np.sin(ros_time.to_sec())
+        sine.header.stamp = ros_time
+        pub.publish(sine)
+        rate.sleep()
