1723b9924e3eeac3e8af49e600b577a6f3f59334
Jin <jinholee727@gmail.com>
2023-03-24 01:04:24 +0100

    Initial work on analyser

--- workspace/src/plotter/experiment/src/Applications/data_analyser.cpp
+++ workspace/src/plotter/experiment/src/Applications/data_analyser.cpp
@@ -1,6 +1,28 @@
 #include "ros/ros.h"
+#include "sync_msgs/SyncQ.h"
+
+void stateCallback(const sync_msgs::SyncQ::ConstPtr& msg)
+{
+
+}
 
 int main(int argc, char** argv)
 {
+    ros::init(argc, argv, "control_analyzer");
+    ros::NodeHandle n;
+
+    int rate;
+    ros::param::get("~rate", rate);
+    ROS_INFO_STREAM("Rate: " << rate);
+
+    ros::Rate loop(rate);
+
+    ros::Subscriber state_sub = n.subscribe("state_sync", 100, stateCallback);
+
+    while(ros::ok()){
+        ros::spinOnce();
+        loop.sleep();
+    }
+
     return 0;
 }
